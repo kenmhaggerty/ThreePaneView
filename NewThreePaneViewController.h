ae42be99b9a8614b1be0d8185fbdfb3c4e6be25d
kenmhaggerty <kenmhaggerty@protonmail.com>
2017-01-30 13:32:55 -0500

    Creates New.storyboard and NewThreePaneViewController.

---
+++ NewThreePaneViewController.h
@@ -0,0 +1,20 @@
+//
+//  NewThreePaneViewController.h
+//  ThreePaneView
+//
+//  Created by Ken M. Haggerty on 1/30/17.
+//  Copyright © 2017 Ken M. Haggerty. All rights reserved.
+//
+
+#pragma mark - // NOTES (Public) //
+
+#pragma mark - // IMPORTS (Public) //
+
+#import <UIKit/UIKit.h>
+
+#pragma mark - // NewThreePaneViewController //
+
+#pragma mark Public Interface
+
+@interface NewThreePaneViewController : UIViewController
+@end
---
+++ NewThreePaneViewController.m
@@ -0,0 +1,20 @@
+//
+//  NewThreePaneViewController.m
+//  ThreePaneView
+//
+//  Created by Ken M. Haggerty on 1/30/17.
+//  Copyright © 2017 Ken M. Haggerty. All rights reserved.
+//
+
+#pragma mark - // NOTES (Private) //
+
+#pragma mark - // IMPORTS (Private) //
+
+#import "NewThreePaneViewController.h"
+
+#pragma mark - // NewThreePaneViewController //
+
+#pragma mark Implementation
+
+@implementation NewThreePaneViewController
+@end
