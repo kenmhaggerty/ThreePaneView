8d44dc4bb67452672aded468831b5fbc50ebaaea
kenmhaggerty <kenmhaggerty@protonmail.com>
2017-02-02 04:47:38 -0500

    Implements NewThreePaneViewController.

--- NewThreePaneViewController.h
+++ NewThreePaneViewController.h
@@ -12,9 +12,57 @@
 
 #import <UIKit/UIKit.h>
 
+#pragma mark - // ThreePaneOffset //
+
+#pragma mark Public Constants
+
+typedef enum : NSUInteger {
+    ThreePaneUnitPoints,
+    ThreePaneUnitPercent
+} ThreePaneOffsetUnit;
+
+typedef enum : NSUInteger {
+    ThreePaneDirectionIn,
+    ThreePaneDirectionOut
+} ThreePaneOffsetDirection;
+
+#pragma mark Public Interface
+
+@interface ThreePaneOffset : NSObject
+@property (nonatomic) CGFloat value;
+@property (nonatomic) ThreePaneOffsetUnit units;
+@property (nonatomic) ThreePaneOffsetDirection direction;
+
+// INITS //
+
+- (id)initWithOffset:(CGFloat)offset units:(ThreePaneOffsetUnit)units direction:(ThreePaneOffsetDirection)direction NS_DESIGNATED_INITIALIZER;
+- (id)init NS_UNAVAILABLE;
+
+@end
+
 #pragma mark - // NewThreePaneViewController //
 
 #pragma mark Public Interface
 
-@interface NewThreePaneViewController : UIViewController
+@interface NewThreePaneViewController : UIViewController <UIScrollViewDelegate>
+@property (nonatomic, strong) IBOutlet UIScrollView *horizontalScrollView;
+@property (nonatomic, strong) IBOutlet UIScrollView *verticalScrollView;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem *leftButton;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem *rightButton;
+@property (nonatomic, getter=sideViewIsOpen) BOOL sideViewOpen;
+@property (nonatomic, getter=topViewIsOpen) BOOL topViewOpen;
+@property (nonatomic) ThreePaneOffset *horizontalOffset;
+@property (nonatomic) BOOL bouncesTop;
+@property (nonatomic) BOOL bouncesBottom;
+@property (nonatomic) BOOL bouncesOpen;
+@property (nonatomic) BOOL bouncesClosed;
+@property (nonatomic) CGFloat keyboardHeight;
+@property (nonatomic) CGSize scrollPadding;
+
+// SETTERS //
+
+- (void)setSideViewOpen:(BOOL)sideViewOpen animated:(BOOL)animated;
+- (void)setTopViewOpen:(BOOL)sideViewOpen animated:(BOOL)animated;
+- (void)setKeyboardHeight:(CGFloat)keyboardHeight animated:(BOOL)animated;
+
 @end
--- NewThreePaneViewController.m
+++ NewThreePaneViewController.m
@@ -12,9 +12,455 @@
 
 #import "NewThreePaneViewController.h"
 
+#pragma mark - // ThreePaneOffset //
+
+#pragma mark Implementation
+
+@implementation ThreePaneOffset
+
+#pragma mark // Inits and Loads //
+
+- (id)initWithOffset:(CGFloat)offset units:(ThreePaneOffsetUnit)units direction:(ThreePaneOffsetDirection)direction {
+    self = [super init];
+    if (self) {
+        self.value = offset;
+        self.units = units;
+        self.direction = direction;
+    }
+    return self;
+}
+
+@end
+
+#pragma mark - // UIView (Subview) //
+
+@interface UIView (Subview)
+@property (nonatomic, weak, readonly) UIView *firstResponder;
+- (void)iterateOverSubviews:(BOOL (^)(UIView *))block;
+- (BOOL)eventuallyContainsSubview:(UIView *)view;
+@end
+
+#pragma mark Implementation
+
+@implementation UIView (Subview)
+
+#pragma mark // Setters and Getters (Subview) //
+
+- (UIView *)firstResponder {
+    __block UIView *firstResponder;
+    [self iterateOverSubviews:^BOOL(UIView *subview) {
+        firstResponder = subview.isFirstResponder ? subview : nil;
+        return (firstResponder == nil);
+    }];
+    
+    return firstResponder;
+}
+
+#pragma mark // Category Methods (Subview) //
+
+- (void)iterateOverSubviews:(BOOL (^)(UIView *))block {
+    NSMutableOrderedSet *subviews = [[NSMutableOrderedSet alloc] initWithArray:self.subviews];
+    UIView *subview;
+    BOOL iterate = YES;
+    while (subviews.count && iterate) {
+        subview = subviews.firstObject;
+        iterate = block(subview);
+        if (subview.subviews.count) {
+            [subviews addObjectsFromArray:subview.subviews];
+        }
+        [subviews removeObject:subview];
+    }
+}
+
+- (BOOL)eventuallyContainsSubview:(UIView *)view {
+    if (!view) {
+        return NO;
+    }
+    
+    __block BOOL containsSubview;
+    [self iterateOverSubviews:^BOOL(UIView *subview) {
+        containsSubview = [view isEqual:subview];
+        return !containsSubview;
+    }];
+    return containsSubview;
+}
+
+@end
+
+#pragma mark - // PassThroughScrollView //
+
+#pragma mark Public Interface
+
+@interface PassThroughScrollView : UIScrollView
+@end
+
+#pragma mark Implementation
+
+@implementation PassThroughScrollView
+
+#pragma mark // Overwritten Methods //
+
+// via StackOverflow:
+// http://stackoverflow.com/questions/3834301/ios-forward-all-touches-through-a-view
+- (BOOL)pointInside:(CGPoint)point withEvent:(UIEvent *)event {
+    for (UIView *view in self.subviews) {
+        if (!view.hidden && [view pointInside:[self convertPoint:point toView:view] withEvent:event]) {
+            return YES;
+        }
+    }
+    return NO;
+}
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+    return YES;
+}
+
+@end
+
 #pragma mark - // NewThreePaneViewController //
+
+#pragma mark Private Interface
+
+@interface NewThreePaneViewController () <UITextFieldDelegate>
+@property (nonatomic, strong) IBOutlet UIView *mainViewContainer;
+@property (nonatomic, strong) IBOutlet UIView *sideViewContainer;
+@property (nonatomic, strong) IBOutlet UIView *topViewContainer;
+@property (nonatomic, strong) IBOutlet UINavigationBar *navigationBar;
+@property (nonatomic, strong) IBOutlet UIView *visibleArea;
+@property (nonatomic, strong) IBOutlet NSLayoutConstraint *constraintHorizontalOffset;
+@property (nonatomic, strong) IBOutlet NSLayoutConstraint *constraintKeyboardHeight;
+//@property (nonatomic, strong) UIPanGestureRecognizer *horizontalPanGesture;
+//@property (nonatomic, strong) UIPanGestureRecognizer *verticalPanGesture;
+@property (nonatomic) NSTimeInterval animationDuration;
+@property (nonatomic) BOOL viewHasLoaded;
+
+// SETUP //
+
+- (void)setup;
+- (void)teardown;
+
+// IBACTIONS //
+
+- (IBAction)touchUpInside:(id)sender;
+
+// GESTURES //
+
+- (void)scrollViewDidPan:(UIPanGestureRecognizer *)gestureRecognizer;
+
+// OBSERVERS //
+
+- (void)addObserversToKeyboard;
+- (void)removeObserversFromKeyboard;
+
+// RESPONDERS //
+
+- (void)keyboardFrameWillChange:(NSNotification *)notification;
+- (void)keyboardWillDisappear:(NSNotification *)notification;
+
+// OTHER //
+
+- (BOOL)directionForVelocity:(CGFloat)velocity withMinimum:(CGFloat)minimum andPosition:(CGFloat)position;
+
+@end
 
 #pragma mark Implementation
 
 @implementation NewThreePaneViewController
+
+#pragma mark // Setters and Getters (Public) //
+
+@synthesize horizontalOffset = _horizontalOffset;
+
+- (void)setSideViewOpen:(BOOL)sideViewOpen {
+    [self setSideViewOpen:sideViewOpen animated:NO];
+}
+
+- (void)setTopViewOpen:(BOOL)topViewOpen {
+    [self setTopViewOpen:topViewOpen animated:NO];
+}
+
+- (void)setHorizontalOffset:(ThreePaneOffset *)horizontalOffset {
+    _horizontalOffset = horizontalOffset;
+    
+    CGFloat offset = 0.0f;
+    switch (horizontalOffset.units) {
+        case ThreePaneUnitPoints:
+            switch (horizontalOffset.direction) {
+                case ThreePaneDirectionIn:
+                    offset = CGRectGetWidth(self.horizontalScrollView.frame)-horizontalOffset.value;
+                    break;
+                case ThreePaneDirectionOut:
+                    offset = horizontalOffset.value;
+                    break;
+            }
+            break;
+        case ThreePaneUnitPercent:
+            switch (horizontalOffset.direction) {
+                case ThreePaneDirectionIn:
+                    offset = (1.0f-horizontalOffset.value)*CGRectGetWidth(self.horizontalScrollView.frame);
+                    break;
+                case ThreePaneDirectionOut:
+                    offset = horizontalOffset.value*CGRectGetWidth(self.horizontalScrollView.frame);
+                    break;
+            }
+            break;
+    }
+    self.constraintHorizontalOffset.constant = offset;
+    [self.view setNeedsUpdateConstraints];
+    [self.view layoutIfNeeded];
+}
+
+- (ThreePaneOffset *)horizontalOffset {
+    if (_horizontalOffset) {
+        return _horizontalOffset;
+    }
+    
+    self.horizontalOffset = [[ThreePaneOffset alloc] initWithOffset:80.0f units:ThreePaneUnitPoints direction:ThreePaneDirectionIn];
+    return self.horizontalOffset;
+}
+
+- (void)setKeyboardHeight:(CGFloat)keyboardHeight {
+    [self setKeyboardHeight:keyboardHeight animated:NO];
+}
+
+- (CGFloat)keyboardHeight {
+    return self.constraintKeyboardHeight.constant;
+}
+
+#pragma mark // Inits and Loads //
+
+- (void)dealloc {
+    [self teardown];
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    
+    [self setup];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self.horizontalScrollView addGestureRecognizer:[[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(scrollViewDidPan:)]];
+    [self.verticalScrollView addGestureRecognizer:[[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(scrollViewDidPan:)]];
+}
+
+- (void)viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    
+    CGFloat topInset = CGRectGetMinY(self.visibleArea.frame);
+    CGFloat bottomInset = CGRectGetHeight(self.visibleArea.superview.frame)-CGRectGetMaxY(self.visibleArea.frame);
+    
+    self.verticalScrollView.contentInset = UIEdgeInsetsMake(topInset, 0.0f, bottomInset, 0.0f);
+    self.verticalScrollView.scrollIndicatorInsets = UIEdgeInsetsMake(topInset, 0.0f, bottomInset, 0.0f);
+    [self.verticalScrollView setNeedsUpdateConstraints];
+    
+    [UIView animateWithDuration:(self.viewHasLoaded ? 0.18f : 0.0f) animations:^{
+        [self.verticalScrollView layoutIfNeeded];
+    }];
+}
+
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    
+    if (self.viewHasLoaded) {
+        return;
+    }
+    
+    CGFloat topInset = CGRectGetMinY(self.visibleArea.frame);
+    CGFloat bottomInset = CGRectGetHeight(self.visibleArea.superview.frame)-CGRectGetMaxY(self.visibleArea.frame);
+    
+    self.verticalScrollView.contentInset = UIEdgeInsetsMake(topInset, 0.0f, bottomInset, 0.0f);
+    self.verticalScrollView.scrollIndicatorInsets = UIEdgeInsetsMake(topInset, 0.0f, bottomInset, 0.0f);
+    self.topViewOpen = NO;
+    
+    self.horizontalOffset = self.horizontalOffset;
+    
+    self.sideViewOpen = NO;
+    
+    self.viewHasLoaded = YES;
+}
+
+#pragma mark // Public Methods (Setters) //
+
+- (void)setSideViewOpen:(BOOL)sideViewOpen animated:(BOOL)animated {
+    _sideViewOpen = sideViewOpen;
+    self.leftButton.title = sideViewOpen ? @"Done" : @"Side";
+    CGFloat contentOffSetX = sideViewOpen ? 0.0f : CGRectGetMinX(self.verticalScrollView.frame);
+    [UIView animateWithDuration:(animated ? 0.33f : 0.0f) delay:0.0f options:UIViewAnimationOptionBeginFromCurrentState animations:^{
+        self.horizontalScrollView.contentOffset = CGPointMake(contentOffSetX-self.horizontalScrollView.contentInset.left, self.horizontalScrollView.contentOffset.y);
+    } completion:nil];
+}
+
+- (void)setTopViewOpen:(BOOL)topViewOpen animated:(BOOL)animated {
+    _topViewOpen = topViewOpen;
+    self.rightButton.title = topViewOpen ? @"Done" : @"Top";
+    CGFloat contentOffsetY = topViewOpen ? 0.0f : CGRectGetMinY(self.navigationBar.frame);
+    [UIView animateWithDuration:(animated ? 0.33f : 0.0f) delay:0.0f options:UIViewAnimationOptionBeginFromCurrentState animations:^{
+        self.verticalScrollView.contentOffset = CGPointMake(self.verticalScrollView.contentOffset.x, contentOffsetY-self.verticalScrollView.contentInset.top);
+    } completion:nil];
+}
+
+- (void)setKeyboardHeight:(CGFloat)keyboardHeight animated:(BOOL)animated {
+    self.constraintKeyboardHeight.constant = keyboardHeight;
+    [self.view setNeedsUpdateConstraints];
+    [UIView animateWithDuration:self.animationDuration delay:0.0f options:UIViewAnimationOptionCurveEaseOut animations:^{
+        [self.view layoutIfNeeded];
+    } completion:nil];
+}
+
+#pragma mark // Delegated Methods (UIScrollViewDelegate) //
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    if ([scrollView isEqual:self.horizontalScrollView]) {
+        if (!self.bouncesOpen) {
+            [self.horizontalScrollView setContentOffset:CGPointMake(fmaxf(self.horizontalScrollView.contentOffset.x, -1.0f*self.horizontalScrollView.contentInset.left), self.horizontalScrollView.contentOffset.y)];
+        }
+        if (!self.bouncesClosed) {
+            [self.horizontalScrollView setContentOffset:CGPointMake(fminf(self.horizontalScrollView.contentOffset.x, CGRectGetMinX(self.verticalScrollView.frame)-self.horizontalScrollView.contentInset.left), self.horizontalScrollView.contentOffset.y)];
+        }
+    }
+    if ([scrollView isEqual:self.verticalScrollView]) {
+        if (!self.bouncesTop) {
+            [self.verticalScrollView setContentOffset:CGPointMake(self.verticalScrollView.contentOffset.x, fmaxf(self.verticalScrollView.contentOffset.y, -1.0f*self.verticalScrollView.contentInset.top))];
+        }
+        if (!self.bouncesBottom) {
+            [self.verticalScrollView setContentOffset:CGPointMake(self.verticalScrollView.contentOffset.x, fminf(self.verticalScrollView.contentOffset.y, CGRectGetMinY(self.mainViewContainer.frame)-self.verticalScrollView.contentInset.top))];
+        }
+    }
+}
+
+- (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView {
+    UIView *firstResponder = scrollView.firstResponder;
+    if (!firstResponder || ![scrollView eventuallyContainsSubview:firstResponder]) {
+        return;
+    }
+    
+    CGRect firstResponderRect = [scrollView convertRect:firstResponder.frame fromView:firstResponder.superview];
+    CGRect insetRect = CGRectInset(firstResponderRect, -1.0f*self.scrollPadding.width, -1.0f*self.scrollPadding.height);
+    [scrollView scrollRectToVisible:insetRect animated:YES];
+}
+
+#pragma mark // Delegated Methods (UITextFieldDelegate) //
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    if ([string stringByTrimmingCharactersInSet:[NSCharacterSet newlineCharacterSet].invertedSet].length) {
+        [textField resignFirstResponder];
+        return NO;
+    }
+    
+    return YES;
+}
+
+#pragma mark // Private Methods (Setup) //
+
+- (void)setup {
+    self.bouncesTop = YES;
+    self.bouncesBottom = NO;
+    self.bouncesOpen = YES;
+    self.bouncesClosed = NO;
+    self.scrollPadding = CGSizeMake(0.0f, 8.0f);
+    
+    self.viewHasLoaded = NO;
+    
+    [self addObserversToKeyboard];
+}
+
+- (void)teardown {
+    [self removeObserversFromKeyboard];
+}
+
+#pragma mark // Private Methods (IBActions) //
+
+- (IBAction)touchUpInside:(id)sender {
+    if ([sender isEqual:self.leftButton]) {
+        [self setSideViewOpen:!self.sideViewIsOpen animated:YES];
+        return;
+    }
+    
+    if ([sender isEqual:self.rightButton]) {
+        [self setTopViewOpen:!self.topViewIsOpen animated:YES];
+        return;
+    }
+}
+
+#pragma mark // Private Methods (Gestures) //
+
+- (void)scrollViewDidPan:(UIPanGestureRecognizer *)gestureRecognizer {
+    CGPoint velocity = [gestureRecognizer velocityInView:self.visibleArea];
+    CGPoint location = [gestureRecognizer locationInView:self.visibleArea];
+    BOOL horizontalEnabled, verticalEnabled;
+    switch (gestureRecognizer.state) {
+        case UIGestureRecognizerStateBegan:
+            horizontalEnabled = (fabs(velocity.x) > fabs(velocity.y));
+            self.horizontalScrollView.scrollEnabled = horizontalEnabled;
+            verticalEnabled = (fabs(velocity.x) < fabs(velocity.y));
+            self.verticalScrollView.scrollEnabled = verticalEnabled;
+            break;
+        case UIGestureRecognizerStateEnded:
+        case UIGestureRecognizerStateCancelled:
+        case UIGestureRecognizerStateFailed:
+            if (self.horizontalScrollView.scrollEnabled) {
+                BOOL sideViewOpen = [self directionForVelocity:velocity.x withMinimum:10.0f andPosition:location.x/CGRectGetWidth(self.visibleArea.frame)];
+                [self setSideViewOpen:sideViewOpen animated:YES];
+            }
+            if (self.verticalScrollView.scrollEnabled) {
+                BOOL topViewOpen = [self directionForVelocity:velocity.y withMinimum:10.0f andPosition:location.y/CGRectGetHeight(self.visibleArea.frame)];
+                [self setTopViewOpen:topViewOpen animated:YES];
+            }
+            self.horizontalScrollView.scrollEnabled = YES;
+            self.verticalScrollView.scrollEnabled = YES;
+        default:
+            break;
+    }
+}
+
+#pragma mark // Private Methods (Observers) //
+
+- (void)addObserversToKeyboard {
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardFrameWillChange:) name:UIKeyboardWillChangeFrameNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillDisappear:) name:UIKeyboardWillHideNotification object:nil];
+}
+
+- (void)removeObserversFromKeyboard {
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillChangeFrameNotification object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
+}
+
+#pragma mark // Private Methods (Responders) //
+
+- (void)keyboardFrameWillChange:(NSNotification *)notification {
+    CGRect frame = [notification.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    NSTimeInterval animationDuration = [notification.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    
+    self.animationDuration = animationDuration;
+    [self setKeyboardHeight:CGRectGetHeight(frame) animated:YES];
+}
+
+- (void)keyboardWillDisappear:(NSNotification *)notification {
+    NSTimeInterval animationDuration = [notification.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    
+    self.animationDuration = animationDuration;
+    [self setKeyboardHeight:0.0f animated:YES];
+}
+
+#pragma mark // Private Methods (Other) //
+
+- (BOOL)directionForVelocity:(CGFloat)velocity withMinimum:(CGFloat)minimum andPosition:(CGFloat)position {
+    if (fabs(velocity) < minimum) {
+        return (position >= 0.5f);
+    }
+    
+    return (velocity > 0);
+}
+
 @end
